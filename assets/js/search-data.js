f20fffabf95bd0b6fed6d7b28ca01e3ae2bae8ea
mcket747econ <59522800+mcket747econ@users.noreply.github.com>
2025-09-25 04:01:14 +0000

    Deploying to gh-pages from @ mcket747econ/mcket747econ.github.io@236764cffff8e633909c690333660b49d958711b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-about",
-          title: "about",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/";
-          },
-        },{id: "nav-research",
+  },{id: "nav-research",
           title: "research",
           description: "An overview of my current and past research projects",
           section: "Navigation",
